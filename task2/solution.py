b0c0f3c5a502dc6ec5589a88ebab85ed0481cce6
Тимур Даянов <tima.skye@yandex.ru>
2025-06-17 23:33:27 +0500

    init

--- task2/solution.py
+++ task2/solution.py
@@ -40,8 +40,7 @@ def parse_animals():
 
     animals = {}
     current_url = base_url
-    current_letter = None
-    current_count = 0
+    printed_letters = set()
 
     while current_url:
         response = session.get(current_url)
@@ -50,21 +49,14 @@ def parse_animals():
         found_russian = False
         for letter in sorted(page_animals):
             count = page_animals[letter]
-            found_russian = True
-            if letter != current_letter:
-                if current_letter is not None:
-                    print(f'всего записей на букву "{current_letter}" - {current_count}')
-                    animals[current_letter] = current_count
+            animals[letter] = animals.get(letter, 0) + count
+            if letter not in printed_letters:
                 print(f'Подсчет на букву {letter}')
-                current_letter = letter
-                current_count = count
-            else:
-                current_count += count
+                printed_letters.add(letter)
+            found_russian = True
 
+        # Если на странице не найдено ни одной русской буквы — выходим из цикла
         if not found_russian:
-            if current_letter is not None:
-                print(f'всего записей на букву "{current_letter}" - {current_count}')
-                animals[current_letter] = current_count
             break
 
         next_link = BeautifulSoup(response.text, 'html.parser').find('a', string='Следующая страница')
@@ -72,12 +64,16 @@ def parse_animals():
             current_url = 'https://ru.wikipedia.org' + next_link['href']
             time.sleep(0.1)
         else:
-            if current_letter is not None:
-                print(f'всего записей на букву "{current_letter}" - {current_count}')
-                animals[current_letter] = current_count
             current_url = None
 
+    for letter in sorted(animals):
+        print(f'всего записей на букву "{letter}" - {animals[letter]}')
+
     write_animals_to_csv(animals, 'beasts.csv')
+    print("Подсчет окончен! Данные сохранены в 'beasts.csv' в корневой папке скрипта!")
 
 if __name__ == '__main__':
     parse_animals()
+
+
+
